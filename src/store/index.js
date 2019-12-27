6198d91280a2992db25e6913069ede8514fe34e1
VitaliiKulyk <vkul@eliftech.com>
2019-12-27 04:36:40 +0200

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,52 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+// import axios from 'axios';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {},
+	mutations: {},
+	actions: {
+		loadItems() {
+			console.log('ssd');
+			// axios
+			// 	.get('https://visaya.solutions/en/rest/V1/visaya/search/devices', {
+			// 		params: {
+			// 			'categories[]': 51,
+			// 			current_page: 1,
+			// 			per_page: 5,
+			// 			order: 'desc',
+			// 			order_by: 'score'
+			// 		},
+			// 		// withCredentials: true,
+			// 		// crossorigin: true,
+			// 		headers: {
+			// 			'Content-Type': 'application/x-www-form-urlencoded',
+			// 			Accept: 'application/json'
+			// 			// 'Access-Control-Allow-Origin': '*',
+			// 			//'Content-Type': 'application/json'
+			// 			// 'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS'
+			// 		}
+			// 	})
+			// 	.then(r => {
+			// 		console.log(r);
+			// 	})
+			// 	.catch(e => {
+			// 		console.log({ e });
+			// 	});
+			// // var { data } = response;
+			// // console.log(data);
+
+			const Http = new XMLHttpRequest();
+			const url = 'https://visaya.solutions/en/rest/V1/visaya/search/devices';
+			Http.open('GET', url);
+			Http.send();
+
+			Http.onreadystatechange = () => {
+				console.log(Http.responseText);
+			};
+		}
+	},
+	modules: {}
+});
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	configureWebpack: {
+		devServer: {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+				'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+			}
+		}
+	}
+};
